ef1adf1f4b130e63884dc658f8f127d5d0b5ec32
marcos-dev14 <mprogramador.10@gmail.com>
2024-02-09 07:41:59 -0300

    fix: pequenos ajustes no código

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -4,7 +4,7 @@ import { Link } from "expo-router"
 
 import { useCartStore } from "@/stores/cart-store"
 
-import { CATEGORIES, MENU } from "@/utils/data/products"
+import { CATEGORIES, MENU, ProductProps } from "@/utils/data/products"
 
 import { Header } from "@/components/header"
 import { CategoryButton } from "@/components/category-button"
@@ -14,7 +14,7 @@ export default function Home() {
   const cartStore = useCartStore()
   const [category, setCategory] = useState(CATEGORIES[0])
 
-  const sectionListRef = useRef<SectionList>(null)
+  const sectionListRef = useRef<SectionList<ProductProps>>(null)
 
   const cartQuantityItem = cartStore.products.reduce((total, product) => total + product.quantity, 0)
 
--- src/app/product/[id].tsx
+++ src/app/product/[id].tsx
@@ -1,5 +1,5 @@
 import { View, Image, Text } from "react-native"
-import { useLocalSearchParams, useNavigation } from "expo-router"
+import { useLocalSearchParams, useNavigation, Redirect } from "expo-router"
 import { Feather } from "@expo/vector-icons"
 
 import { useCartStore } from "@/stores/cart-store"
@@ -14,11 +14,17 @@ export default function ProductDetails() {
   const navigation = useNavigation()
   const { id } = useLocalSearchParams()
 
-  const product = PRODUCTS.filter((item) => item.id === id)[0]
+  const product = PRODUCTS.find((item) => item.id === id)
 
   function handleAddToCart() {
-    cartStore.add(product)
-    navigation.goBack()
+    if (product) {
+      cartStore.add(product)
+      navigation.goBack()
+    }
+  }
+
+  if (!product) {
+    return <Redirect href="/" />
   }
   
   return (
@@ -30,6 +36,10 @@ export default function ProductDetails() {
       />
 
       <View className="p-5 mt-8 flex-1">
+        <Text className="text-white text-xl font-heading">
+          {product.title}
+        </Text>
+
         <Text className="text-lime-400 text-2xl font-heading my-2">
           {formatCurrency(product.price)}
         </Text>
